35ea68efd5d2d5898d05d2e4e380424b1e5f92ab
Unknown <daniel.presas@uoit.net>
2018-10-25 13:15:09 -0400

    [NEW] Header files for custom libraries
    Centre string, Characters, Font size, Main, Lower case, Random, sound

---
+++ FontSize.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <windows.h>
+
+void fontSize(int size);
---
+++ GuessGru.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "CentreString.h"
+#include "Characters.h"
+#include "FontSize.h"
+#include "LowerCase.h"
+#include "Random.h"
+#include "Sound.h"
+
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <iomanip>
+#include <stdlib.h>
+#include <windows.h>
+
+#pragma comment(lib, "user32")
